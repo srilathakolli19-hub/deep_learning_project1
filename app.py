5bc318ffc88652f9d36ea0e2f62994c08c3b044c
srilathakolli19-hub <srilathakolli19@gmail.com>
2026-03-09 06:11:09 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,1 +1,35 @@
+from flask import Flask, render_template, request
+import tensorflow as tf
+import numpy as np
+from PIL import Image
 
+app = Flask(__name__)
+
+model = tf.keras.models.load_model("model.h5")
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+@app.route('/predict', methods=['POST'])
+def predict():
+
+    file = request.files['file']
+
+    img = Image.open(file)
+    img = img.resize((128,128))
+
+    img = np.array(img)/255.0
+    img = np.expand_dims(img, axis=0)
+
+    prediction = model.predict(img)
+
+    if prediction[0][0] > 0.5:
+        result = "Tumor Detected"
+    else:
+        result = "No Tumor Detected"
+
+    return render_template("index.html", prediction=result)
+
+if __name__ == "__main__":
+    app.run(debug=True)
